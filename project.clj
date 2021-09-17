5b2bdb19f4e57c8f809324faa498f3860ca14eb0
random char <andy100jones@gmail.com>
2021-09-17 13:16:44 -0400

    first commit of my paint program

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject paint "0.1.0-SNAPSHOT"
+	:description "FIXME: write description"
+	:url "http://example.com/FIXME"
+	:license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+	:dependencies [[org.clojure/clojure "1.10.1"]
+                 [quil "3.1.0"]
+				 [vlaaad/reveal "1.3.212"]
+				 [cider/nrepl "0.3.0"]
+				 
+				 ]
+
+	:main paint.core			 
+	
+)
