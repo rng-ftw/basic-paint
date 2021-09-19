e2ae15e9489101fbafb2d501aeaedac4df57e1a5
random char <andy100jones@gmail.com>
2021-09-19 19:28:09 -0400

    added color buttons with the ability to select on color button and one function button

--- src/paint/core.clj
+++ src/paint/core.clj
@@ -3,19 +3,23 @@
   (:require [quil.core :as q :include-macros true ]
             [quil.middleware :as m]
 			[clojure.java.io :as io]))
-(defn output[string state] (println string state)state) ;used for testing output of function 
+(defn output[string & state] (println string state)state) ;used for testing output of a function 
 (defn run [] (use 'paint.core :reload-all))
 ;(def toggle_state (atom {:file_loaded false}))
+
 (defn line[state]
 (cond
 	(and (not (q/mouse-pressed?))(<= 2(count(peek (:mouse_loc state)))))(conj (state :mouse_loc) []) 
 	(and (state :mouse_state)(not (q/mouse-pressed?))
-		 (not= [(q/mouse-x) (q/mouse-y)](take-last 2(peek(peek (:mouse_loc state))))));checks for dupes
-				(conj (pop (state :mouse_loc)) (conj (peek (state :mouse_loc)) [(:draw_color state)(:draw_thickness state) (q/mouse-x) (q/mouse-y)]))
+		(not= [(q/mouse-x) (q/mouse-y)](take-last 2(peek(peek (:mouse_loc state))))));checks for dupes
+        
+		(conj (pop (state :mouse_loc)) (conj (peek (state :mouse_loc)) [(:draw_color state)(:draw_thickness state) (q/mouse-x) (q/mouse-y)])
+		)
+	
 	:else (state :mouse_loc)
 ))
+
 (defn drawing[state]
-;(println  (:mouse_loc state))
    (cond
      (and (not (state :mouse_state))(seq (peek (state :mouse_loc)))) (conj (state :mouse_loc) []);adds [] to the end to seprate lines drawn
      (and (state :mouse_state) 
@@ -25,12 +29,12 @@
   )
 (defn save[state]
 	(when (not(state :file_saved)) (spit "test.txt" (state :mouse_loc)))
-	;(assoc state :file_saved true) ; Makes sure the files is saved once and not (q/frame-rate) times a second
-	true
-	)
+	true)
 (defn loading[state] 
 	(if (.exists(io/file "test.txt"))
-	(if (not (state :file_loaded))(assoc state :mouse_loc(read-string (slurp "test.txt")):file_loaded true :button_state nil)state)
+		(let [color (:color (state :button_state ))]
+		(println "loading color" color)
+		(if (not (state :file_loaded))(assoc state :mouse_loc(read-string (slurp "test.txt")):file_loaded true :button_state {:func nil :color color })state))
 	state))
 (defn setup []
   (q/frame-rate 60)
@@ -39,43 +43,59 @@
   {
   :file_saved false
   :file_loaded false
-  :button_loc  (mapv #( into [] [0  %1 (%2 0)(%2 1)]) [100 150 200 250](repeat [100 50]))
-  :button_names ["line" "draw" "save" "load"]
-  :button_state nil
+  :button_loc  (mapv #( into [] [0  %1 (%2 0)(%2 1)]) [100 150 200 250 300 350 400 450 500](repeat [100 50]))
+  :button_names ["line" "draw" "save" "load" "red" "green" "blue" "black" "white"]
+  :button_state {:func nil :color nil}
   :mouse_state nil
   :mouse_loc  [[]]; last one needs to be empty [[]] format is [[color width x y]]
   :draw_color 255
   :draw_thickness 15
    })
 (defn which-button[x y button_state ] 
-  (if (and(>= 100 x)(<= 0 x)(<= 100 y)(>= 300 y))
-    (cond ; todo fix this so I dont have to add stuff to multiple places in order to add another button
-       (and (< 100 y)(> 150 y))"line"
-       (and (< 150 y)(> 200 y))"draw"
-       (and (< 200 y)(> 250 y))"save"
-       (and (< 250 y)(> 300 y))"load"
-	   :else nil ;just in case?
-       )
-    button_state
-    ))
+  (if (and(>= 100 x)(<= 0 x)(<= 100 y)(>= 550 y))
+	  (let [f-button  
+		(cond ; todo fix this so I dont have to add stuff to multiple places in order to add another button
+		   (and (< 100 y)(> 150 y))"line"
+		   (and (< 150 y)(> 200 y))"draw"
+		   (and (< 200 y)(> 250 y))"save"
+		   (and (< 250 y)(> 300 y))"load"
+		   :else (button_state :func)
+		   )
+		   
+		    c-button  
+		(cond
+		   (and (< 300 y)(> 350 y))"red"
+		   (and (< 350 y)(> 400 y))"green"
+		   (and (< 400 y)(> 450 y))"blue"
+		   (and (< 450 y)(> 500 y))"black"
+		   (and (< 500 y)(> 550 y))"white"
+		   :else (button_state :color)
+		   )]
+		(assoc button_state :color c-button :func f-button)
+	  )
+	  button_state
+    )
+ )
 (defn update-state [state]
  (cond-> state
+	;switch to color chosen
+	  ;(and (:color(state :button_state)) (not= ((:color(state :button_state)))(state :draw_color)))
 	;draw
-	  (= (state :button_state) "draw") (assoc :mouse_loc (drawing state))
+	  (= (:func(state :button_state)) "draw") (assoc :mouse_loc (drawing state))
 	;line
-	  (= (state :button_state) "line") (assoc :mouse_loc (line state))
+	  (= (:func(state :button_state)) "line") (assoc :mouse_loc (line state))
 	 ;clears screen 
 	  (and (q/key-pressed?) (= (q/key-as-keyword) :c)) (assoc :mouse_loc [[]] ) ;needs to be at the below draw and line to work ???  
 	;load
-	  (= (state :button_state) "load") (loading)
-	  (not= (state :button_state) "load") (assoc :file_loaded false)
+	  (= (:func(state :button_state)) "load") (loading)
+	  (not= (:func(state :button_state)) "load") (assoc :file_loaded false)
 	;save
-	  (and (= (state :button_state) "save")(not(state :file_saved))) (assoc :file_saved (save state))
-	  (not= (state :button_state) "save") (assoc :file_saved false)
-	;stop button click from being added to drawing
-	  (and(q/mouse-pressed?)(not(and(>= 100 (q/mouse-x))(<= 0 (q/mouse-x))(<= 100 (q/mouse-y))(>= 300 (q/mouse-y)))))(assoc :mouse_state true)
+	  (and (= (:func(state :button_state)) "save")(not(state :file_saved))) (assoc :file_saved (save state))
+	  (not= (:func(state :button_state)) "save") (assoc :file_saved false)
+	;stop initial button click from being added to drawing
+	  (and(q/mouse-pressed?)(not(and(>= 100 (q/mouse-x))(<= 0 (q/mouse-x))(<= 100 (q/mouse-y))(>= 550 (q/mouse-y)))))(assoc :mouse_state true)
 	  (not(q/mouse-pressed?)) (assoc :mouse_state false)
-	  ) 
+	  )
 	 ; { 
 	 ; :file_saved  (if (and (= (state :button_state) "save")(not(state :file_saved)))(save state)(state :file_saved))
 	 ; :button_loc (do(:file_saved true )(state  :button_loc))
@@ -114,14 +134,15 @@
   (q/stroke 0)
   (q/stroke-weight 10)
   (q/line 0 0 (q/width)0);draw line at top 
-  
-  (dorun (map #(draw-button (%1 0)(% 1)(% 2)  (% 3) %2 (=(state :button_state)%2 )) (state :button_loc) (state  :button_names)) )
- 
+  (doall  ;function buttons
+	(map #(draw-button (%1 0)(% 1)(% 2) (% 3) %2 
+	(or (= (:func(state :button_state))%2) (= (:color(state :button_state))%2)))
+		(state :button_loc)(state  :button_names) ))
+   
   (q/fill 255 0 0)
   (q/text-align :center :center)
-  (when (q/key-pressed?) (q/text (str "key as keyword:"(q/key-as-keyword)) (q/mouse-x) (+(q/mouse-y)30))   )
-  (when (q/mouse-pressed?) (q/text (str (q/mouse-x)" " (q/mouse-y)) (q/mouse-x) (+ (q/mouse-y) 10)  )) 
-
+  (when (q/key-pressed?) (q/text (str "key as keyword:"(q/key-as-keyword)) (q/mouse-x) (+(q/mouse-y)30)))
+  (when (q/mouse-pressed?) (q/text (str (q/mouse-x)" " (q/mouse-y)) (q/mouse-x) (+ (q/mouse-y) 10))) 
 (when (seq (state :mouse_loc)); needs to enventual be removed
   (doall (map
   #(cond
@@ -132,7 +153,8 @@
  )))
 state)
 (defn click [state m_state]
-(assoc state :button_state (which-button (m_state :x)(m_state :y)(state :button_state)) ) )
+(assoc state :button_state (which-button (m_state :x)(m_state :y)(state :button_state)) )) 
+
 (q/defsketch paint
   :title "draw: hit C to clear screen"
   :size [500 500]
